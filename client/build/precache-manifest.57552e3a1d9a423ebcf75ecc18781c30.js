ef028d8913dd939e1fec0fecea2a97cd8ad270be
Mark Duenas <markgdlduenas@gmail.com>
2020-10-29 13:36:44 -0700

    second commit

---
+++ client/build/precache-manifest.57552e3a1d9a423ebcf75ecc18781c30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cec4bd19f3f39b8f2391149f316bfd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "460cace8a25861407280",
+    "url": "/static/css/main.f3d06af4.chunk.css"
+  },
+  {
+    "revision": "825a65e6344c5bd11db4",
+    "url": "/static/js/2.aa9efce6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.aa9efce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460cace8a25861407280",
+    "url": "/static/js/main.318fafd9.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "fed370fd4e3f7270f88c6e81bf6a24a2",
+    "url": "/static/media/deer.fed370fd.jpg"
+  },
+  {
+    "revision": "0bc791ff8d31161dc95599eab57201f7",
+    "url": "/static/media/mountains.0bc791ff.jpg"
+  }
+]);
